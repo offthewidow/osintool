87dc263561d832bb90c23173938195da1074f197
Samuele Della Vedova <46026355+samueledellavedova@users.noreply.github.com>
2023-03-16 16:26:23 +0100

    refactor: rename positional argument

--- osintool/__main__.py
+++ osintool/__main__.py
@@ -8,11 +8,11 @@ import username
 
 async def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("name")
+  parser.add_argument("target")
   args = parser.parse_args()
 
   async with ClientSession(timeout=ClientTimeout(15)) as session:
-    await username.find_and_print_all(session, args.name)
+    await username.find_and_print_all(session, args.target)
 
 
 if __name__ == "__main__":
