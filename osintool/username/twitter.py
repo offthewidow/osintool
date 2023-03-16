a313e3866a18c2973abe79aaa1435b09cc60a70b
Samuele Della Vedova <46026355+samueledellavedova@users.noreply.github.com>
2023-03-16 16:04:22 +0100

    fix: twitter false positives

--- osintool/username/twitter.py
+++ osintool/username/twitter.py
@@ -4,4 +4,4 @@ from aiohttp import ClientSession
 async def find(session: ClientSession, username: str) -> str | None:
   async with session.get(f"https://api.twitter.com/i/users/username_available.json?username={username}") as res:
     data = await res.json()
-    return f"https://twitter.com/{username}" if not data["valid"] else None
+    return f"https://twitter.com/{username}" if data["reason"] == "taken" else None
